6668f5bdab4eb15f980e780619249a1c88ce2582
Anna Leticia Fernandes <88044397+annaleticiafernandes@users.noreply.github.com>
2024-09-16 16:22:37 -0300

    aula 11

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,7 @@
+// f8 para rodar o node js exec
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
